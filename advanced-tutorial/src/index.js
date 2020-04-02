408224b341e2b89295e338438c23dbe132a5bb82
Samuel Ward <sam.ward@derp.tech>
2020-04-03 10:43:44 +1300

    Implement AdvancedT:Async

--- advanced-tutorial/src/index.js
+++ advanced-tutorial/src/index.js
@@ -1,10 +1,30 @@
+import './index.css';
+import 'babel-polyfill'
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import store from './app/store';
 import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+import { createStore, applyMiddleware } from 'redux';
+import { selectSubreddit, fetchPosts } from './redux/actions'
+import rootReducer from './redux/reducers'
+
+const loggerMiddleware = createLogger()
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(
+    thunkMiddleware,
+    loggerMiddleware
+  )
+)
+
+store.dispatch(selectSubreddit('reactjs'))
+//store.dispatch(fetchPosts('reactjs')).then(() => console.log(store.getState()))
+store
+    .dispatch(fetchPostsIfNeeded('reactjs'))
+    .then(() => console.log(store.getState()))
 
 ReactDOM.render(
   <Provider store={store}>
@@ -12,8 +32,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
